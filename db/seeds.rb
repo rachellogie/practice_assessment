00acfcc89645fbc19fd05574124945ed9f7b406d
Kinsey Ann <kbooboo@kinseys-MacBook-Pro-2.local>
2014-05-19 15:56:10 -0600

    Add names to magazines in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Magazine.create!(api_id: 100)
-Magazine.create!(api_id: 215)
-Magazine.create!(api_id: 100)
-Magazine.create!(api_id: 307)
+Magazine.create!(name: 'Sports Illustrated', api_id: 100)
+Magazine.create!(name: 'Wired', api_id: 215)
+Magazine.create!(name: 'Dog Fancy', api_id: 100)
+Magazine.create!(name: 'Cosmo', api_id: 307)
